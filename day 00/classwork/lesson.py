c06dff00b783ecf05333e211a893e6bfe1837323
SabaKuridze1 <kuridzesaba2014@gmail.com>
2024-05-25 19:17:39 +0400

    goa

---
+++ day 00/classwork/lesson.py
@@ -0,0 +1,10 @@
+question1=input("romel akademiashi swavlob programirebas :")
+question2=input("romel jgufshi xar :")
+question3=input("aris tu ara nika keshelava sheni mentori :")
+
+if question1=="romel akademiashi swavlob programirebas" :
+    print("goa")
+if question2=="romel jgufshi xar" :
+    print("29")
+if question3=="aris tu ara nika keshelava sheni mentori" :
+    print("ki")
