6da664063abe982552902d0ab7fb7a5e6988f837
SabaKuridze1 <kuridzesaba2014@gmail.com>
2024-06-02 21:10:27 +0400

    goa

---
+++ homework/level 4.py
@@ -0,0 +1,40 @@
+number1 = 6
+number2 = 2
+print(number1 + number2)
+print(number1 - number2)
+print(number1 / number2)
+print(number1 * number2)
+
+
+
+
+myage = 10
+parentage = 39
+print((parentage + 10)/(myage + 10))
+
+
+
+print(8+4)
+print(8-4)
+print(8/4)
+print(8*4)
+
+print(20+5)
+print(20-5)
+print(20*5)
+print(20/5)
+
+print(50+5)
+print(50-5)
+print(50/5)
+print(50*5)
+
+print(36+3)
+print(36-3)
+print(36/3)
+print(36*3)
+
+print(90+3)
+print(90-3)
+print(90/3)
+print(90*3)
