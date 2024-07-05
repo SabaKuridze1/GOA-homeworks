e96c46295ffdd7e3c736e663dd19a1158cc7e95a
SabaKuridze1 <kuridzesaba2014@gmail.com>
2024-07-05 21:30:15 +0400

    goa makes u chad

---
+++ Day 08/classwork/cw.py
@@ -0,0 +1,27 @@
+1
+name1 = "---"
+
+print(name1)
+
+
+my_name = "---"
+
+print(my_name)
+
+
+name = "---"
+print(name)
+
+2
+first_number = 10
+second_number = 5
+
+sum = first_number + second_number
+print(sum)
+
+3
+სახელი = "ჩემი სახელი"
+
+გვარი = 23
+
+print(სახელი + გვარი)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
---
+++ Day 08/homework/hw.py
@@ -0,0 +1,21 @@
+Lemur = 45
+Lemur_Mango = 34
+print(Lemur-Lemur_Mango)
+
+print("Hello, world!")
+
+x = 5
+y = 4
+print(x + y)
+
+a = 5
+b = 2
+print(a / b)
+
+x = 5
+y = 10
+z = 15
+print(z)
+
+age = 25
+print(age + 5)
