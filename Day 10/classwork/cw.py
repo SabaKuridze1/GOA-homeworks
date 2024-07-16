6c49d7e67dc9d754be81b1444781b103ccff12f2
SabaKuridze1 <kuridzesaba2014@gmail.com>
2024-07-16 17:56:16 +0400

    goa makes u chad

---
+++ Day 10/classwork/cw.py
@@ -0,0 +1,5 @@
+lemur_food = input("What food does Lemur like?: ")
+print(lemur_food)
+name=input("ur name: ")
+surname=input("ur surname: ")
+print(name+surname)
---
+++ Day 10/homework/hw.py
@@ -0,0 +1,11 @@
+name = input("Enter your name: ")
+print(name)
+surname = input("Enter your surname: ")
+print("Happy Birthday" + name + surname)
+
+
+fav = input("Enter your favorite animal, colour, car, actor: ")
+print("your favorite animal, colour, car, actor is" + fav)
+
+#input/I არის შეყვანა, შეტანა. output/O არის მიღებული შედეგი.
+#concatenation -- ანუ კონკატინაცია არის რაღაცის მიწერა, მიერთება.
