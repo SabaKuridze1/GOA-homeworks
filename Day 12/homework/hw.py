5d715c21d0d78f3aeb91b2a7499c64b62e1fabf9
SabaKuridze1 <kuridzesaba2014@gmail.com>
2024-07-31 14:20:46 +0400

    goa

---
+++ Day 12/homework/hw.py
@@ -0,0 +1,29 @@
+#string/str არის ტექსტი, რომელიც მოთავსებულა ბრჭყალებში ("")
+#integer/int არის მთელი რიცხვი და მას არ სჭირდება ბრჭყალები
+#float არის ათწილადი და არც მას არ სჭირდება ბრჭყალები
+
+#ჩვენ იმიტომ გვჭირება ინფორმაციის დაყოფა მონაცემთა ტიპებად, რომ მონაცემები იყოს სხვადასხვა. მაგ. ტექსტური მონაცემი და რიცხვული მონაცემი
+
+print("Mom " + "Dad")
+print("Brother " + "Sister")
+print("Woman " + "Mam")
+print("Sun " + "Star")
+print("Galaxy " + "Universe")
+print("People " + "Earth")
+print("Aliens " + "Mars")
+print("US " + "America")
+print("Samsung " + "Apple")
+print("PC " + "Laptop")
+print("Dog " + "Cat")
+print("Mouse " + "Keabord")
+print("Mouse " + "Hamster")
+print("Cow " + "Milk")
+print("white " + "Cheese")
+print("Milkyway " + "Andromeda")
+print("Sadness " + "Joy")
+print("Love " + "Hate")
+print("Easter " + "Christmas")
+print("Chicken " + "Chick")
+
+name = input("enter your name: ")
+print(name)
