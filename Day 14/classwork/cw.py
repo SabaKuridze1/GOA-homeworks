f1c8dd26712dcc4e15191d4f0a2321204d04b55e
SabaKuridze1 <kuridzesaba2014@gmail.com>
2024-08-19 13:46:24 +0400

    goa

---
+++ Day 14/classwork/cw.py
@@ -0,0 +1,24 @@
+#შექმენით 5 ცვლადი და თითოეულში შეინახეთ განსხვავებული ინფორმაცია. გამოიყენეთ type ბრძანება 5-ვე ცვლადზე და დაბეჭდეთ რა შედეგსაც დააბრუნებს type.
+name = "Saba"
+num1=20
+num2=20.34
+list1=(1, 2)
+num3=24/3
+print(type(name))
+print(type(num1))
+print(type(num2))
+print(type(num3))
+print(type(list1))
+
+
+num0 = "25"
+num1=20
+num2=20.34
+list1=(1, 2)
+num3=24/3
+
+print (int(num0))
+print (str(num1))
+print (str(num2))
+print (str (list1))
+print (str(num3))
