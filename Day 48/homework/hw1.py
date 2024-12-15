1163821ce3f8aa001a4add78ea83df563b5960e0
SabaKuridze1 <kuridzesaba2014@gmail.com>
2024-12-15 11:21:15 +0400

    goa

--- Day 48/homework/hw1.py
+++ Day 48/homework/hw1.py
@@ -1,3 +1,16 @@
 def human_years_cat_years_dog_years(human_years):
-    # i am sorry magram piroba?!! vin dawera saertod
-    pass
+    cat = 0
+    dog = 0
+    
+    if human_years == 1:
+        cat = 15
+        dog = 15
+    elif human_years == 2:
+        cat = 24
+        dog = 24
+        
+    else: 
+        cat = 4 * (human_years - 2) + 24
+        dog = 5 * (human_years - 2) + 24
+        
+    return [human_years, cat, dog]
