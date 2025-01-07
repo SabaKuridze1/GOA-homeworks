30bb578094d78688710e3b7805d778a094babba0
SabaKuridze1 <kuridzesaba2014@gmail.com>
2025-01-07 19:09:17 +0400

    goa

--- Day 36/homework/hw10.py
+++ Day 36/homework/hw10.py
@@ -1,3 +1,16 @@
 # 10) დაწერე ფუნქცია, რომელიც იღებს სიას და აბრუნებს მასში მხოლოდ მარტივ რიცხვებს.
 
-# es ver gavige
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+def get_prime_numbers(numbers):
+    prime_numbers = []
+    for num in numbers:
+      if is_prime(num):
+        prime_numbers.append(num)
+    return prime_numbers
