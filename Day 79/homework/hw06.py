262a2247ceb06b82a5d2f597f50a18fdafb70b72
SabaKuridze1 <kuridzesaba2014@gmail.com>
2025-04-03 17:45:44 +0400

    goa

---
+++ Day 79/homework/hw06.py
@@ -0,0 +1,6 @@
+"""6) შექმენით 3 ცვლადები სადაც შეინახავთ სხვადასხვა მოცანემთა ტიპებს და გაიგეთ მატი ტიპი, (type() ფუნქციის გამოყენებით."""
+variable1 = "jskaasdajkfh"
+variable2 = 123
+variable3 = True
+
+print(type(variable1), type(variable2), type(variable3))
